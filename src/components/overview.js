6a1724b98f5116d5b03763ef393b2b3465af3792
Vivek Sahu <viveksahu1762@gmail.com>
2024-07-28 12:57:55 +0530

    Added balance of

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,9 +1,51 @@
+import { Contract } from "ethers";
 import { SendButton } from "./sendPopup";
+import { ERC20ABI, erc20ContractAddress } from "@/contract";
+import { getInstance, getSignature } from "@/utils/fhEVM";
+import { CoinsIcon, PiggyBankIcon } from "lucide-react";
+import { useState } from "react";
+import Loader from "./loader";
 
 const { default: Image } = require("next/image");
 const { Button } = require("./ui/button");
 
 export const Overview = ({ w0, data, setData }) => {
+  const [balanceLoading, setBalanceLoading] = useState(false);
+  const [balance, setBalance] = useState("0");
+  const balanceOfEncryptedErc20 = async () => {
+    setBalanceLoading(true);
+    try {
+      const provider = await w0?.getEthersProvider();
+      const signer = await provider?.getSigner();
+      const encryptedERC20 = new Contract(
+        erc20ContractAddress,
+        ERC20ABI,
+        signer
+      );
+      const fhevmInstance = await getInstance();
+      console.log(fhevmInstance);
+      const token = await getSignature(
+        erc20ContractAddress,
+        w0.address,
+        fhevmInstance
+      );
+      const balance = await encryptedERC20.balanceOf(
+        token.publicKey,
+        token.signature,
+        w0.address
+      );
+      // console.log(balance);
+      const decrytedBalance = await fhevmInstance
+        .decrypt(erc20ContractAddress, balance.toString())
+        .toString();
+
+      setBalance(decrytedBalance);
+      setBalanceLoading(false);
+    } catch (error) {
+      setBalanceLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <div className="mt-8 px-8">
       <p className="text-4xl font-semibold">Overview</p>
@@ -12,14 +54,26 @@ export const Overview = ({ w0, data, setData }) => {
         <div className="w-full grid grid-cols-2">
           <div className="border-r w-full flex  items-center justify-center">
             <div>
-              <p className="text-lg font-medium text-[#71737F]">
-                Total Balance:
-              </p>
+              <div className="flex items-center gap-3">
+                <p className="text-lg font-medium text-[#71737F]">
+                  Total Balance:
+                </p>
+                <CoinsIcon onClick={balanceOfEncryptedErc20} />
+              </div>
+
               <div>
-                <div className="flex items-center">
-                  <div className="text-8xl font-semibold text-primary">450</div>
-                  <div className="text-8xl font-semibold text-primary">$</div>
-                </div>
+                {balanceLoading ? (
+                  <div className="h-28 grid place-items-center">
+                    <Loader />
+                  </div>
+                ) : (
+                  <div className="flex items-center">
+                    <div className="text-8xl font-semibold text-primary">
+                      {balance}
+                    </div>
+                    <div className="text-8xl font-semibold text-primary">$</div>
+                  </div>
+                )}
 
                 <SendButton w0={w0} data={data} setData={setData} />
               </div>
---
+++ src/contract/index.js
@@ -0,0 +1,5 @@
+import { erc20ABI } from "./erc20";
+
+export const ERC20ABI = erc20ABI;
+export const erc20ContractAddress =
+  "0x72576523674ff9C780651c3a36d9ffc082A185dD";
